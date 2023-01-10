61d638d441d67fe0aafd492b070ced5e48fd33ef
Neil Poulin <neil.j.poulin@gmail.com>
2023-01-09 23:44:03 -0700

    update index.d.ts to add mising title prop

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,20 @@
-declare module "boring-avatars" {
+declare module 'boring-avatars' {
+  import React from 'react';
+
   export interface AvatarProps {
     size?: number | string;
     name?: string;
     square?: boolean;
-    variant?: "marble" | "beam" | "pixel" | "sunset" | "ring" | "bauhaus";
+    variant?: 'marble' | 'beam' | 'pixel' | 'sunset' | 'ring' | 'bauhaus';
     colors?: string[];
+    title?: boolean;
   }
 
   interface AvatarComponent {
     (props: AvatarProps, context?: any): React.ReactElement | null;
   }
 
-  const Avatar: AvatarComponent
+  const Avatar: AvatarComponent;
 
   export default Avatar;
 }
